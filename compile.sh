7d37797b87bb7139e6d1e450a4348e166a3be39f
PaddingProductions <shine00chang@gmail.com>
2022-09-28 00:45:03 -0700

    compile.sh edits

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
+if [ ! -d './out' ]; then
+	echo './out directory not found, creating ./out directory...'
+       	mkdir ./out
+fi	      
+
 echo 'Compiling all ".cpp" files in "./src" folder with gcc, storing executable in "out/main"...' 
 g++ -o out/main ./src/*.cpp
 echo 'Done'
